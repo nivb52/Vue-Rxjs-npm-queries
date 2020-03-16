912a71c14f3d976b283549c5ee33437845547822
niv b <nivb52@gmail.com>
2020-03-17 01:20:21 +0200

    use the ajax - print the version and name

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,31 @@
   <div id="app">
     <input type="text" v-model="term" />
     <button v-stream:click="click$">Load Data</button>
-  
+    <h1>
+      {{name$}}
+      <sup>{{version$}}</sup>
+    </h1>
+    <ul class="tree">
+      <li
+        v-for="(version, name) in dependencies$"
+        :key="name"
+      >
+        {{name}}
+        <sup>{{version}}</sup>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import { interval, from } from "rxjs";
+import { from } from "rxjs";
 import {
   pipe,
   pluck,
   switchMap,
+  map,
+  of,
   catchError,
-  tap
 } from "rxjs/operators";
 
 export default {
@@ -25,16 +38,16 @@ export default {
     };
   },
   subscriptions() {
-    const BASE_URL =
-      "https://cors-anywhere.herokuapp.com/https://registry.npmjs.org/";
+    const BASE_URL = "https://cors-anywhere.herokuapp.com/https://registry.npmjs.org/";
+
     const createLoader = url => from(this.$http.get(url)).pipe(pluck("data"));
     const package$ = n => createLoader(`${BASE_URL}${n}/latest`);
+
 
     const fullData$ = this.click$
       .pipe(
         pluck("data"),
         switchMap(() => package$(this.$data.term)),
-        tap(data => console.log(data)),
         // HANDLE AN ERROR
         catchError(err => {
           console.log("somemthing went wrong...", err);
@@ -42,8 +55,15 @@ export default {
           // return throwError(err);
         })
       )
+
+    const name$ = fullData$.pipe(pluck("name"));
+    const version$ = fullData$.pipe(pluck("version"));
+    const dependencies$ = fullData$.pipe(pluck("dependencies"));
+
     return {
-    fullData$
+      name$,
+      version$,
+      dependencies$,
     };
   }
 };
@@ -54,8 +74,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
 }
 </style>
