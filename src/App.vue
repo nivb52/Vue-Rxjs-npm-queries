6f259fd40222079a24c4c71161ddb2ece63bf8d8
niv b <nivb52@gmail.com>
2020-03-17 01:15:03 +0200

    get the data with rxjs

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,52 @@
 <template>
-    <div id="app">
+  <div id="app">
     <input type="text" v-model="term" />
-
+    <button v-stream:click="click$">Load Data</button>
+  
   </div>
-
 </template>
 
 <script>
+import { interval, from } from "rxjs";
+import {
+  pipe,
+  pluck,
+  switchMap,
+  catchError,
+  tap
+} from "rxjs/operators";
 
 export default {
-  name: 'App',
+  name: "App",
+  domStreams: ["click$"],
   data() {
     return {
       term: "express"
     };
   },
   subscriptions() {
-    const BASE_URL = "https://cors-anywhere.herokuapp.com/https://registry.npmjs.org/";
+    const BASE_URL =
+      "https://cors-anywhere.herokuapp.com/https://registry.npmjs.org/";
     const createLoader = url => from(this.$http.get(url)).pipe(pluck("data"));
     const package$ = n => createLoader(`${BASE_URL}${n}/latest`);
 
-    
+    const fullData$ = this.click$
+      .pipe(
+        pluck("data"),
+        switchMap(() => package$(this.$data.term)),
+        tap(data => console.log(data)),
+        // HANDLE AN ERROR
+        catchError(err => {
+          console.log("somemthing went wrong...", err);
+          of(`Bad Promise: ${error}`);
+          // return throwError(err);
+        })
+      )
+    return {
+    fullData$
+    };
   }
-}
-
+};
 </script>
 
 <style>
