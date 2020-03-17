9290a71b7fb18b04e1776fbffaeaa6a520be4e9b
niv b <nivb52@gmail.com>
2020-03-17 03:04:08 +0200

    center the app

--- src/App.vue
+++ src/App.vue
@@ -189,6 +189,9 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
   margin: 60px auto;
+  display: flex;
+  justify-content: center;
+  box-sizing: border-box;
 }
 
 ul.tree {
