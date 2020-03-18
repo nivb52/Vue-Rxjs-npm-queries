f18551a00d45cde4b454e9f205c9fcdfe2db3574
niv b <nivb52@gmail.com>
2020-03-18 03:22:46 +0200

    add buefy

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+
+import App from './App.vue';
 
 // import Rx from 'rxjs';
 import VueRx from 'vue-rx';
@@ -7,16 +8,21 @@ import VueRx from 'vue-rx';
 import axios from 'axios';
 import VueAxios from 'vue-axios';
 
-
 Vue.use(VueRx);
 Vue.use(VueAxios, axios);
 
+// UI
+import 'buefy/dist/buefy.css';
+import { Toast } from 'buefy'
+Vue.use(Toast)
+//
 
 
+import './registerServiceWorker';
 // import store from './store'
-// import './registerServiceWorker'
-Vue.config.productionTip = false
+
+Vue.config.productionTip = false;
 new Vue({
   // store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
