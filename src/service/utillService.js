a15cb4a9a5ca57b4113a0f8fdb6a3cd1771187be
niv b <nivb52@gmail.com>
2020-03-18 03:30:34 +0200

    extract packge info (name and version)

---
+++ src/service/utillService.js
@@ -0,0 +1,29 @@
+const extractPackageInfo = data => {
+    let name, version;
+    if (typeof data === 'string') {
+      [name, version] = data.split(' ');
+    } else {
+      name = data.name;
+      version = data.version;
+    }
+    //modify for ajax
+    name = name.trim();
+    if (!version) {
+      version = 'latest';
+    } else {
+      const isSpecialSign = str => {
+          return ['@','~','^'].find( sign => version.search(sign) > -1)
+      }
+      const startStrFrom = isSpecialSign ? 1 : 0
+      version = version.substr(startStrFrom);
+      // add 0 before '.' to make search easier
+      if (version.substr(0, 1) === '.') {
+        version = '0' + version;
+      }
+    }
+  
+    return [name, version];
+  };
+  
+  export default extractPackageInfo
+  
